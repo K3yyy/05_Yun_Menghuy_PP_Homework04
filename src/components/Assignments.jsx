9d54076ba3eb13240f275f023406227e12499703
Yun Menghuy <yunmenghuy33@gmail.com>
2025-03-06 17:51:42 +0700

    add a div

--- src/components/Assignments.jsx
+++ src/components/Assignments.jsx
@@ -3,6 +3,7 @@ export default function AssignmentsComponent() {
     <div>
       <div className="flex justify-between">
         <h2 className="text-md font-semibold">Assignments</h2>
+        <h2 className="text-md font-semibold">Assignments</h2>
       </div>
     </div>
   );
